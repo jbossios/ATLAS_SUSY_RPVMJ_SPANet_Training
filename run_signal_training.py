d61ca45b811d9107869015553d70e4d861680ddf
jbossios <bossjonad@gmail.com>
2022-03-11 08:01:36 +0100

    Add option to get predictions

--- run_signal_training.py
+++ run_signal_training.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-  run_type = 'train' # options: train, test, predict
+  run_type = 'predict' # options: train, test, predict
   path = '/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/spanet_jona/SPANET_OptionFiles/'
   versions = [i for i in range(72, 92)]
   commands = []
@@ -11,6 +11,13 @@ if __name__ == '__main__':
     for infile in os.listdir(inpath):
       if run_type == 'train':
         command = f'python3 train.py -of options_files/{infile} --random_seed 1 --gpus 1 > Log_v{version}_{run_type} 2>&1'
+      elif run_type == 'predict':
+        with open(f'options_files/{infile}', 'r') as opt:
+          lines = opt.readlines()
+          for line in lines:
+            if 'testing' in line and 'h5' in line:
+              h5_file = line.split(':')[1].replace(',' ,'').replace('"', '')
+        command = f'python3 predict.py ./spanet_output/version_{version} ./signal_full_v{version}_output.h5 -tf{h5_file} --gpus > Log_v{version}_{run_type} 2>&1'
       else:
         print(F'ERROR: {run_type} not implemented yet, exiting')
         sys.exit(1)
@@ -18,3 +25,4 @@ if __name__ == '__main__':
   command = ' && '.join(commands)
   command += ' &'
   print(command)
+  os.system(command)
